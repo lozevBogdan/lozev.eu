b007cd36f086ea0a36b6332fb74674fcf605629a
Bogdan Lozev <lozev.bogdan@gmail.com>
2025-03-09 16:38:55 +0200

    Messages sending changed temporary.

--- src/main/resources/static/js/javaScript.js
+++ src/main/resources/static/js/javaScript.js
@@ -3,3 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.add("loaded");
 });
 
+function sendEmail(event) {
+    event.preventDefault();
+
+    const from = document.getElementById('from').value;
+    const about = document.getElementById('about').value;
+    const message = document.getElementById('text-message').value;
+
+    const subject = encodeURIComponent(about);
+    const body = encodeURIComponent(`From: ${from}\n\nMessage:\n${message}`);
+
+    const mailtoLink = `mailto:lozev.bogdan@gmail.com?subject=${subject}&body=${body}`;
+
+    window.location.href = mailtoLink;
+    }
+
